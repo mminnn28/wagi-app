cf1a544bb34bc763747477d0805e686b025dc172
mminnn28 <mminnn28@gmail.com>
2024-10-02 01:40:59 +0900

    first

---
+++ src/main/java/wagi_app/wagi/WagiApplication.java
@@ -0,0 +1,13 @@
+package wagi_app.wagi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wagi_app/wagi/WagiApplicationTests.java
@@ -0,0 +1,13 @@
+package wagi_app.wagi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
