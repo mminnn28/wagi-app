2d72aa0006fe0f988274e6201703ef7f6ae34856
mminnn28 <mminnn28@gmail.com>
2025-01-08 20:03:12 +0900

    :sparkles: Feature: 합격자만 회원가입 가능
    이전엔 모든 학번이 회원가입 가능했으나, 수정 후에는 admittedUsers 테이블에 등록되어있는 학번만 가입 가능하도록 했습니다.

---
+++ src/main/java/wagi_app/wagi/repository/AdmittedUsersRepository.java
@@ -0,0 +1,11 @@
+package wagi_app.wagi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import wagi_app.wagi.entity.AdmittedUsers;
+
+@Repository
+public interface AdmittedUsersRepository extends JpaRepository<AdmittedUsers, String> {
+    boolean existsByAdmittedId(String admittedId);
+
+}
